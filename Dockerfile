fe7d847a0ea093241b94a84390205c7ed4822985
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2023-05-19 17:41:28 +0300

    Add ability to use postgress

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,13 @@
-# build stage
-FROM node:16-slim AS build
+FROM node:16 AS build
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
 COPY package*.json ./
 
-RUN npm ci
+RUN npm i
 
 COPY . .
 
-RUN npm run build
-
-RUN npm prune --omit=dev
-
-# main stage
-FROM gcr.io/distroless/nodejs16-debian11 AS main
-
-WORKDIR /usr/src/app
-
-COPY --from=build /usr/src/app/node_modules/ /usr/src/app/node_modules/ 
-COPY --from=build /usr/src/app/dist/ /usr/src/app/
-
 EXPOSE 3000
 
-CMD ["main.js"]
+CMD ["npm", "run", "start:debug"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                return {
+                    type: 'postgres',
+                    port: +configService.get('POSTGRES_PORT'),
+                    username: configService.get('POSTGRES_USER'),
+                    password: configService.get('POSTGRES_PASSWORD'),
+                    database: configService.get('POSTGRES_DB'),
+                    host: configService.get('POSTGRES_HOST'),
+                    synchronize: true,
+                    autoLoadEntities: true
+                };
+            }
+        }),
+
+        UsersModule
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
