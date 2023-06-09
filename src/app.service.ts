3a59201fabb6c5a7942b3b692021bd70b1390abd
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2023-06-09 15:47:47 +0300

    Add deployment via docker compose

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     ping(): string {
+        console.log('!!!!!NODE ENV', process.env.NODE_ENV);
         return 'Ping!';
     }
 }
