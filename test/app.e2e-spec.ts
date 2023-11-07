501f383fd8f42d1cb2a83bb0631110b97bde44a2
Artsiom Shushkevich <artemshushkevich@gmail.com>
2023-11-07 19:56:02 +0300

    test: Fix autotests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, Module } from '@nestjs/common';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+
+const moduleMocker = new ModuleMocker(global);
+
+jest.mock('@nestjs/typeorm', () => {
+    @Module({})
+    class MockModule {}
+
+    return {
+        ...jest.requireActual('@nestjs/typeorm'),
+        InjectRepository: () => jest.fn(),
+        TypeOrmModule: {
+            forRootAsync: jest.fn().mockImplementation(() => MockModule),
+            forFeature: jest.fn().mockImplementation(() => MockModule)
+        }
+    };
+});
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
@@ -9,7 +26,18 @@ describe('AppController (e2e)', () => {
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule]
-        }).compile();
+        })
+            .useMocker((token) => {
+                if (typeof token === 'function') {
+                    const mockMetadata = moduleMocker.getMetadata(
+                        token
+                    ) as MockFunctionMetadata<any, any>;
+                    const Mock =
+                        moduleMocker.generateFromMetadata(mockMetadata);
+                    return new Mock();
+                }
+            })
+            .compile();
 
         app = moduleFixture.createNestApplication();
         await app.init();
