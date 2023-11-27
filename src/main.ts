79a98162575bcc53c4e87b5d58525489b58f3de2
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2023-11-27 16:35:57 +0300

    fix: Enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { AppModule } from './app.module';
 const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
-
     app.setGlobalPrefix('api/v1');
+    app.enableCors();
 
     await app.listen(process.env.PORT || 3000);
 };
